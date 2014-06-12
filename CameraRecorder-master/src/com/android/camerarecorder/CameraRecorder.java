a3c149550265efa836b618ffa17b11bca56e5021
Namil <namilabeywickrama@gmail.com>
2014-06-12 11:48:58 +0530

    Added Timer to record video

--- CameraRecorder-master/src/com/android/camerarecorder/CameraRecorder.java
+++ CameraRecorder-master/src/com/android/camerarecorder/CameraRecorder.java
@@ -1,12 +1,10 @@
 package com.android.camerarecorder;
 
-import java.io.File;
-import java.io.IOException;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -18,29 +16,27 @@ public class CameraRecorder extends Activity implements SurfaceHolder.Callback {
 	public static SurfaceHolder mSurfaceHolder;
 	public static Camera mCamera;
 	public static boolean mPreviewRunning;
-
+	
 	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		/**
-		 * This is normal statements for an activity
-		 * */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        /**
+         * This is normal statements for an activity
+         * */
 		mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView1);
 		mSurfaceHolder = mSurfaceView.getHolder();
 		mSurfaceHolder.addCallback(this);
 		mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
+		
 		// initializing 2 buttons, Start recording and stop recording.
-
+		
 		Button btnStart = (Button) findViewById(R.id.StartService);
-		btnStart.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				// Create new service using CameraRecorder class and Start
-				// service
-				Intent intent = new Intent(CameraRecorder.this,
-						RecorderService.class);
+		btnStart.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				//Create new service using CameraRecorder class and Start service
+				Intent intent = new Intent(CameraRecorder.this, RecorderService.class);
 				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				startService(intent);
 				finish();
@@ -48,23 +44,22 @@ public class CameraRecorder extends Activity implements SurfaceHolder.Callback {
 		});
 
 		Button btnStop = (Button) findViewById(R.id.StopService);
-		btnStop.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				// Stop CameraRecorder Service
-				stopService(new Intent(CameraRecorder.this,RecorderService.class));
+		btnStop.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				//Stop CameraRecorder Service
+				stopService(new Intent(CameraRecorder.this, RecorderService.class));
 			}
 		});
-	}
-
+    }
+    
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 	}
 
 	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 	}
-
+	
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 	}
--- CameraRecorder-master/src/com/android/camerarecorder/RecorderService.java
+++ CameraRecorder-master/src/com/android/camerarecorder/RecorderService.java
@@ -1,14 +1,23 @@
 package com.android.camerarecorder;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
 import android.hardware.Camera.Size;
 import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.SurfaceHolder;
@@ -22,14 +31,17 @@ public class RecorderService extends Service {
 	private static Camera mServiceCamera;
 	private boolean mRecordingStatus;
 	private MediaRecorder mMediaRecorder;
-	
+
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	public static final int MEDIA_TYPE_VIDEO = 2;
+
 	@Override
 	public void onCreate() {
 		mRecordingStatus = false;
 		mServiceCamera = CameraRecorder.mCamera;
 		mSurfaceView = CameraRecorder.mSurfaceView;
 		mSurfaceHolder = CameraRecorder.mSurfaceHolder;
-		
+
 		super.onCreate();
 	}
 
@@ -38,14 +50,15 @@ public class RecorderService extends Service {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
 		Log.d("Namil Recoder", "Start r12321313ing");
-		if (mRecordingStatus == false)
+		if (mRecordingStatus == false){
 			startRecording();
-
+			startTimerRecord();
+		}
 		return START_STICKY;
 	}
 
@@ -53,28 +66,31 @@ public class RecorderService extends Service {
 	public void onDestroy() {
 		stopRecording();
 		mRecordingStatus = false;
-		
-		super.onDestroy();
-	}   
 
-	public boolean startRecording(){
+		super.onDestroy();
+	}
+
+	public boolean startRecording() {
 		try {
 			Log.d("Namil Recoder", "Start recoding");
-			
-			//Toast.makeText(getBaseContext(), "Recording Started", Toast.LENGTH_SHORT).show();
-			
+
+			// Toast.makeText(getBaseContext(), "Recording Started",
+			// Toast.LENGTH_SHORT).show();
+
 			mServiceCamera = Camera.open();
 			Camera.Parameters params = mServiceCamera.getParameters();
 			mServiceCamera.setParameters(params);
 			Camera.Parameters p = mServiceCamera.getParameters();
 			Log.d("Namil Recoder", "Start recoding1");
-			
+
 			final List<Size> listSize = p.getSupportedPreviewSizes();
-			Log.d("Namil Recoder", "Start recoding2"+listSize.size());
+			Log.d("Namil Recoder", "Start recoding2" + listSize.size());
 			Size mPreviewSize = listSize.get(0);
-			/*Log.v(TAG, "use: width = " + mPreviewSize.width 
-						+ " height = " + mPreviewSize.height);*/
-			
+			/*
+			 * Log.v(TAG, "use: width = " + mPreviewSize.width + " height = " +
+			 * mPreviewSize.height);
+			 */
+
 			p.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
 			p.setPreviewFormat(PixelFormat.YCbCr_420_SP);
 			mServiceCamera.setParameters(p);
@@ -82,14 +98,21 @@ public class RecorderService extends Service {
 			try {
 				mServiceCamera.setPreviewDisplay(mSurfaceHolder);
 				mServiceCamera.startPreview();
-			}
-			catch (IOException e) {
+				mServiceCamera.setPreviewCallback(new PreviewCallback() {
+
+					@Override
+					public void onPreviewFrame(byte[] arg0, Camera arg1) {
+						// TODO Auto-generated method stub
+						Log.d("namil-preview cam", "byte aarrrrrrrrrrrrrrrrrrr");
+					}
+				});
+			} catch (IOException e) {
 				Log.e(TAG, e.getMessage());
 				e.printStackTrace();
 			}
-			
+
 			mServiceCamera.unlock();
-			
+
 			mMediaRecorder = new MediaRecorder();
 			mMediaRecorder.setCamera(mServiceCamera);
 			mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
@@ -97,18 +120,21 @@ public class RecorderService extends Service {
 			mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
 			mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
 			mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
-			mMediaRecorder.setOutputFile("/sdcard/video.mp4");
+			mMediaRecorder.setOutputFile(getOutputMediaFile(MEDIA_TYPE_VIDEO)
+					.toString());
+			// mMediaRecorder.setOutputFile("/sdcard/video.mp4");
 			mMediaRecorder.setVideoFrameRate(30);
-			mMediaRecorder.setVideoSize(mPreviewSize.width, mPreviewSize.height);
+			mMediaRecorder
+					.setVideoSize(mPreviewSize.width, mPreviewSize.height);
 			mMediaRecorder.setPreviewDisplay(mSurfaceHolder.getSurface());
-			
+
 			mMediaRecorder.prepare();
 			Log.d("Namil Recoder", "Initialization finished");
-			mMediaRecorder.start(); 
+			mMediaRecorder.start();
 			Log.d("Namil Recoder", "Recodig started");
-			
+
 			mRecordingStatus = true;
-			
+
 			return true;
 		} catch (IllegalStateException e) {
 			Log.d(TAG, e.getMessage());
@@ -122,7 +148,8 @@ public class RecorderService extends Service {
 	}
 
 	public void stopRecording() {
-		Toast.makeText(getBaseContext(), "Recording Stopped", Toast.LENGTH_SHORT).show();
+		Toast.makeText(getBaseContext(), "Recording Stopped",
+				Toast.LENGTH_SHORT).show();
 		try {
 			mServiceCamera.reconnect();
 		} catch (IOException e) {
@@ -131,11 +158,87 @@ public class RecorderService extends Service {
 		}
 		mMediaRecorder.stop();
 		mMediaRecorder.reset();
-		
+
 		mServiceCamera.stopPreview();
 		mMediaRecorder.release();
-		
+
 		mServiceCamera.release();
 		mServiceCamera = null;
+	}
+
+	/** Create a file Uri for saving an image or video */
+	private static Uri getOutputMediaFileUri(int type) {
+		return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(int type) {
+		// To be safe, you should check that the SDCard is mounted
+		// using Environment.getExternalStorageState() before doing this.
+
+		File mediaStorageDir = new File(
+				Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"NamilCameraApp");
+		// This location works best if you want the created images to be shared
+		// between applications and persist after your app has been uninstalled.
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d("MyCameraApp", "failed to create directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		File mediaFile;
+		if (type == MEDIA_TYPE_IMAGE) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "IMG_" + timeStamp + ".jpg");
+		} else if (type == MEDIA_TYPE_VIDEO) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "VID_" + timeStamp + ".mp4");
+		} else {
+			return null;
+		}
+
+		return mediaFile;
+	}
+
+	TimerTask scanTask;
+	final Handler handler = new Handler();
+	Timer t = new Timer();
+	int counter = 0;
+
+	public void startTimerRecord() {
+
+		scanTask = new TimerTask() {
+			public void run() {
+				handler.post(new Runnable() {
+					public void run() {
+						// TextView tw=(TextView) findViewById(R.id.textView1);
+						// tw.setText("time: "+counter);
+						counter++;
+						stopRecording();
+						startRecording();
+						Log.d("TIMER", "Timer set off");
+					}
+				});
+			}
+		};
+
+		t.schedule(scanTask, 300, 60000);
+
+	}
+
+	public void stopTimerRecord() {
+
+		if (scanTask != null) {
+			Log.d("TIMER", "timer canceled");
+			scanTask.cancel();
+			stopRecording();
+		}
+
 	}
 }
